e7b794bce5ec7f81efc5ea238c242f9b5b3c1995
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2022-10-12 18:52:00 +0530

    Added solution - LeetHub

---
+++ Save Your Life - GFG/save-your-life.cpp
@@ -0,0 +1,97 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+        string maxSum(string w,char x[], int b[],int n){
+          // code here    
+          
+            unordered_map<char, int> mp;
+          
+            for(int i=0; i<n; i++)
+            {
+                mp[x[i]] = b[i];
+            }
+          
+            int maxTillNow = 0;
+            int globalMax = INT_MIN;
+            
+            string ans = "";
+            string temp = "";
+          
+            for(int i=0; i<w.size(); i++)
+            {
+              
+                if(mp.find(w[i]) != mp.end())
+                {
+                    maxTillNow += mp[w[i]];
+                    temp += w[i];
+                    
+                    if(maxTillNow > globalMax)
+                    {
+                        ans = temp;
+                        globalMax = maxTillNow;
+                    }
+                    
+                    if(maxTillNow < 0)
+                    {
+                        maxTillNow = 0;
+                        temp = "";
+                    }
+                }
+                else
+                {
+                    maxTillNow += (int)w[i];
+                    temp += w[i];
+                    
+                    if(maxTillNow > globalMax)
+                    {
+                        ans = temp;
+                        globalMax = maxTillNow;
+                    }
+                    
+                    if(maxTillNow < 0)
+                    {
+                        maxTillNow = 0;
+                        temp = "";
+                    }  
+                  
+                }
+            }
+          
+          
+          
+          return ans;
+          
+      }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string w;
+        cin>>w;
+        int n;
+        cin>>n;
+        char x[n];
+        int b[n];
+        for(int i = 0;i<n;i++)
+            cin>>x[i];
+        for(int i = 0;i<n;i++)
+            cin>>b[i];
+        Solution ob;
+        cout << ob.maxSum(w,x,b,n) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
