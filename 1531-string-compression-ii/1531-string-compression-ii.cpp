075146e20aa526967dea6eab08297d520dba760e
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2022-10-15 11:31:53 +0530

    Time: 76 ms (66.67%), Space: 26.2 MB (31.82%) - LeetHub

---
+++ 1531-string-compression-ii/1531-string-compression-ii.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    
+    const int MAX = 127;
+    
+    inline int find_len(int x)
+    {
+        if(x == 1)
+        {
+            return 0;
+        }
+        else if(x < 10)
+        {
+            return 1;
+        }
+        else if(x < 100)
+        {
+            return 2;
+        }
+        else if(x < 1000)
+        {
+            return 3;
+        }
+        return 4;
+    }
+    
+    int solve(int index, int k, string &s, int n, vector<vector<int>> &dp)
+    {
+        if(k < 0)
+        {
+            return MAX;
+        }
+        
+        if(index >= n or n - index <= k)
+        {
+            return 0;
+        }
+        
+        if(dp[index][k] != -1)
+        {
+            return dp[index][k];
+        }
+        
+        vector<int> freq(26, 0);
+        int most_freq = 0;
+        int ans = MAX;
+        
+        for(int j=index; j<n; j++)
+        {
+            most_freq = max(most_freq, ++freq[s[j] - 'a']);
+            ans = min(ans, 1 + find_len(most_freq) + solve(j+1, k-(j-index+1-most_freq), s, n, dp) );
+        }
+        
+        return dp[index][k] = ans;
+        
+    }
+    
+    int getLengthOfOptimalCompression(string s, int k) {
+        
+        vector<vector<int>> dp(101, vector<int>(101, -1));
+        
+        int n = s.size();
+        
+        return solve(0, k, s, n, dp);
+        
+    }
+};
