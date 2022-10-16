14b3c8f8dd27f22e8aab21aa42018c72122b4e5d
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2022-10-16 12:36:57 +0530

    Time: 7 ms (63.60%), Space: 11.9 MB (99.91%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -3,6 +3,8 @@ public:
     void nextPermutation(vector<int>& nums) {
         
         int n = nums.size();
+        
+        //if only one number is present, return as it is
         if(n <= 1)
         {
             return;
@@ -10,6 +12,7 @@ public:
         
         int i, j;
         
+        //find the position where the transformation must be done
         for(i = n-2; i>=0; i--)
         {
             if(nums[i] < nums[i+1])
@@ -18,12 +21,13 @@ public:
             }
         }
         
+        // if it below 0, reverse the whole array 
         if(i < 0)
         {
             reverse(nums.begin(), nums.end());
         }
         else
-        {
+        {   //find the number whoch should be swapped with that number
             for(j = n-1; j>i; j--)
             {
                 if(nums[j] > nums[i])
@@ -32,10 +36,19 @@ public:
                 }
             }
             
+            //swap both numbers
             swap(nums[i], nums[j]);
             
+            //reverse it to get the array
             reverse(nums.begin() + i + 1, nums.end());
         }
+        
+        
+        //example - [1,3,5,4,2]
+        // i=1
+         // j = 3
+        // swap -> [1,4,5,3,2]
+        //reverse -> [1,4,2,3,5]
         
     }
 };
