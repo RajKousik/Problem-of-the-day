bf8e1a4e5b3d24f328a4ace2b752e64400aac0ea
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2022-10-17 19:34:51 +0530

    Time: 6 ms (47.48%), Space: 6.5 MB (82.91%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,15 +1,43 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
+  
+//         unordered_set<char> st;
         
-        set<char> st;
+//         for(auto c : sentence)
+//         {
+//             st.insert(c);        
+//         }
+        
+        //the below line do what exactly the above lines does
+        
+//         unordered_set<char> st(sentence.begin(), sentence.end());
+        
+//         return st.size() == 26;
+        
+        
+        //SC - O(1)
+        
+        //using a variable for storing thier apperance of a alphabet
+        // 11111111111111111111111111 - zyxw...edcba
+        
+        int bitHash = 0;
         
         for(auto c : sentence)
         {
-            st.insert(c);
+            int bit = c - 'a';
+            
+            int num = 1 << bit;
+            
+            bitHash = bitHash | num;
+            
         }
         
-        return st.size() == 26;
+        //now we have to check that bitHash contains 26 ones or not
+        // for that first left 1 by 26 and subtract one which gives 11111111111111111111111111
+        //if equal it is true, else false
+        
+        return bitHash == (1<<26)-1;
         
     }
 };
