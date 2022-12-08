b1ed0a4c348af525c3943a0c85c40cb3f685ef87
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2022-12-08 19:55:46 +0530

    Time: 7 ms (59.99%), Space: 12.8 MB (27.66%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -12,7 +12,39 @@
 class Solution {
 public:
     
-    void dfs(TreeNode* root, vector<int> &t)
+//     void dfs(TreeNode* root, vector<int> &t)
+//     {
+//         if(!root)
+//         {
+//             return;
+//         }
+        
+//         if(!root->left and !root->right)
+//         {
+//             t.push_back(root->val);
+//         }
+//         if(root->left)
+//         {
+//             dfs(root->left, t);
+//         }
+//         if(root->right)
+//         {
+//             dfs(root->right, t);
+//         }
+//     }
+    
+//     bool leafSimilar(TreeNode* root1, TreeNode* root2)
+//     {
+//         vector<int> t1;
+//         vector<int> t2;
+        
+//         dfs(root1, t1);
+//         dfs(root2, t2);
+        
+//         return t1 == t2;
+//     }
+    
+    void dfs(TreeNode* root, queue<int> &q)
     {
         if(!root)
         {
@@ -21,26 +53,48 @@ public:
         
         if(!root->left and !root->right)
         {
-            t.push_back(root->val);
+            // cout<<root->val<<" ";
+            q.push(root->val);
         }
         if(root->left)
         {
-            dfs(root->left, t);
+            dfs(root->left, q);
         }
         if(root->right)
         {
-            dfs(root->right, t);
+            dfs(root->right, q);
         }
     }
     
-    bool leafSimilar(TreeNode* root1, TreeNode* root2)
+     bool leafSimilar(TreeNode* root1, TreeNode* root2)
+     {
+        queue<int> q1;
+        
+        dfs(root1, q1);
+         
+        return check(root2, q1) and q1.empty();
+     }
+    
+    bool check(TreeNode* root, queue<int> &q)
     {
-        vector<int> t1;
-        vector<int> t2;
-        
-        dfs(root1, t1);
-        dfs(root2, t2);
-        
-        return t1 == t2;
+        if(!root->left and !root->right)
+        {
+            int tmp = q.front();
+            q.pop();
+            cout<<tmp<<" "<<root->val<<endl;
+            return tmp == root->val;
+        }
+        bool result1=true, result2=true;
+        if(root->left)
+        {
+            result1 = check(root->left, q);
+        }
+        if(root->right)
+        {
+            result2 = check(root->right, q);
+        }
+        return result1 and result2;
     }
+
+
 };
