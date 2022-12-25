b68cdf363ae0fbcd18dcfd2cd4b57bb27317b46a
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2022-12-25 20:00:51 +0530

    Time: 18 ms (93.30%), Space: 13.8 MB (65.60%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
@@ -1,6 +1,35 @@
 class Solution {
 public:
+    
+    int find(vector<int> &arr, int ele)
+    {
+        int low=0, high=arr.size()-1;
+        int ans = -1;
+        
+        while(low<=high)
+        {
+            int mid = (low + high)/2;
+            
+            if(arr[mid] <= ele)
+            {
+                ans = mid; 
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return ans + 1;
+    }
+    
+    
     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        
+        
+        //sort 
+        //find prefix sum
+        //do binary search for each query[i] and find the no of elements
         
         sort(nums.begin(), nums.end());
         
