1f760494d1dbfb71b32cbe4dae4400182a83631c
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2022-12-30 20:48:15 +0530

    Time: 368 ms (83.92%), Space: 80.5 MB (43.55%) - LeetHub

--- 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.cpp
+++ 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.cpp
@@ -2,28 +2,23 @@ class Solution {
 public:
     
     int dfs(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &cost, vector<vector<int>> &dp)
-    {
-        
+    {   
         if(i == grid.size() - 1)
         {
             return grid[i][j];
         }
-        
         if(dp[i][j] != -1)
         {
             return dp[i][j];
         }
         
-        int r = cost.size();
         int c = cost[0].size();
-        
-        int curr = grid[i][j];
+        int currValue = grid[i][j];
         
         int ans = INT_MAX;
         for(int y = 0; y < c; y++)
         {
-            int temp = grid[i][j] + cost[curr][y] + dfs(i+1, y, grid, cost, dp);
-            // cout<<temp<<endl;
+            int temp = currValue + cost[currValue][y] + dfs(i+1, y, grid, cost, dp);
             ans = min(ans, temp);
         }
         
@@ -34,7 +29,6 @@ public:
     int minPathCost(vector<vector<int>>& grid, vector<vector<int>>& moveCost) {
         
         int ans = INT_MAX;
-        
         int m = grid.size();
         int n = grid[0].size();
         
@@ -42,9 +36,7 @@ public:
         
         for(int j=0; j<n; j++)
         {
-            int currChar = grid[0][j];
             int val =  dfs(0, j, grid, moveCost, dp);
-            // cout<<endl<<endl<<ans<<" "<<val<<endl;
             ans = min(ans, val);
         }
         
