098b4660ec1976d27f3c79b18b21af654dc35fc6
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2023-01-04 22:01:01 +0530

    Time: 62 ms (32.30%), Space: 13.6 MB (59.56%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,26 +1,88 @@
 class Solution {
 public:
     
-    bool dfs(int node, int currColor, vector<vector<int>>& graph, vector<int> &color)
+        //dfs function for bipartite
+    
+//     bool dfs(int node, int currColor, vector<vector<int>>& graph, vector<int> &color)
+//     {
+        
+//         color[node] = currColor; 
+        
+//         for(auto it : graph[node])
+//         {
+//             if(color[it] == -1)
+//             {
+//                 if(dfs(it,  1- currColor, graph, color) == false)
+//                 {
+//                     return false;
+//                 }
+//             }
+//             else if(color[it] == currColor)
+//             {
+//                 return false;
+//             }
+//         }
+        
+//         return true;
+        
+//     }
+    
+    
+//     bool isBipartite(vector<vector<int>>& graph) {
+        
+//         int n = graph.size();
+        
+//         vector<int> color(n, -1);
+        
+//         for(int i=0; i<n; i++)
+//         {
+//             if(color[i] == -1)
+//             {
+//                 if(dfs(i, 0, graph, color) == false)
+//                 {
+//                     for(auto it : color) cout<<it<<endl;
+//                     return false;
+//                 }
+//             }
+//         }
+        
+        
+        
+//         return true;
+        
+        
+//     }
+    
+    
+        //bfs function for bipartite
+    
+    bool bfs(int startNode, int currColor, vector<vector<int>>& graph, vector<int> &color)
     {
+        queue<int> q;
+        q.push(startNode);
         
-        color[node] = currColor; 
+        color[startNode] = currColor;
         
-        for(auto it : graph[node])
+        while(!q.empty())
         {
-            if(color[it] == -1)
+            
+            auto node = q.front();
+            q.pop();
+            
+            for(auto it : graph[node])
             {
-                if(dfs(it,  1- currColor, graph, color) == false)
+                if(color[it] == -1)
+                {
+                    color[it] = !color[node];
+                    q.push(it);
+                }
+                else if(color[it] == color[node])
                 {
                     return false;
                 }
             }
-            else if(color[it] == currColor)
-            {
-                return false;
-            }
+
         }
-        
         return true;
         
     }
@@ -36,7 +98,7 @@ public:
         {
             if(color[i] == -1)
             {
-                if(dfs(i, 0, graph, color) == false)
+                if(bfs(i, 0, graph, color) == false)
                 {
                     for(auto it : color) cout<<it<<endl;
                     return false;
@@ -50,4 +112,5 @@ public:
         
         
     }
+    
 };
