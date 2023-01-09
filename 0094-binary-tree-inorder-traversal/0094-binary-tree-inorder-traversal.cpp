84654609b8e9538fb6597de57731a3488ed19a73
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2023-01-09 19:10:04 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (76.35%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,25 +11,60 @@
  */
 class Solution {
 public:
+        
+        //recurisve approach
+//     void helper(TreeNode* root, vector<int> &nodes)
+//     {
+//         if(!root) return;
+        
+//         helper(root->left, nodes);
+//         nodes.push_back(root->val);
+//         helper(root->right, nodes);
+        
+//     }
     
-    void helper(TreeNode* root, vector<int> &nodes)
-    {
-        if(!root) return;
+    
+//     vector<int> inorderTraversal(TreeNode* root) {
         
-        helper(root->left, nodes);
-        nodes.push_back(root->val);
-        helper(root->right, nodes);
+//         vector<int> nodes;
         
+//         helper(root, nodes);
+        
+//         return nodes;
+        
+//     }
+    
+        //iterative approach
+        vector<int> inorderTraversal(TreeNode* root)
+        {
+        
+            vector<int> ans;
+
+            stack<TreeNode*> st;
+            // st.push(root);
+            
+            TreeNode* curr = root;
+            
+            while(curr!=NULL or !st.empty())
+            {
+                
+                while(curr != NULL)
+                {
+                    st.push(curr);
+                    curr = curr->left;
+                }
+                    
+                curr = st.top();
+                st.pop();
+                
+                ans.push_back(curr->val);
+                
+                curr = curr->right;
+                
+            }
+
+            return ans;
+
     }
-    
-    
-    vector<int> inorderTraversal(TreeNode* root) {
-        
-        vector<int> nodes;
-        
-        helper(root, nodes);
-        
-        return nodes;
-        
-    }
+
 };
