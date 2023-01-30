b7e50d676855d866afbfa3eb0cddf714a9643d5e
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2023-01-30 19:06:55 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (73.21%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        
+        if(n == 0)
+        {
+            return 0;
+        }
+        
+        if(n == 1 or n == 2) return 1;
+        
+        
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        
+        int sum = 0;
+        for(int i=3; i<=n; i++)
+        {
+            sum = a + b + c;
+            a = b;
+            b = c;
+            c = sum;  
+        }
+        
+        return sum;
+        
+    }
+};
