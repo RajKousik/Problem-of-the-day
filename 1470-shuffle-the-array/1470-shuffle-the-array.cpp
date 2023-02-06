df86847a878aefb114a8151a20a7d303507ac54b
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2023-02-06 19:14:13 +0530

    Time: 7 ms (62.32%), Space: 9.7 MB (61.93%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -2,16 +2,53 @@ class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
         
-        vector<int> ans(2*n);
-        int i=0;
-        int j=n; 
-        int k=0;
-        while(k < 2 *n)
+        //O(N) approach
+        
+        // vector<int> ans(2*n);
+        // int i=0;
+        // int j=n; 
+        // int k=0;
+        // while(k < 2 *n)
+        // {
+        //     ans[k++] = (nums[i++]);
+        //     ans[k++] = (nums[j++]);
+        // }
+        // return ans;
+        
+        
+        
+        
+        // O(1) approach
+        
+        //the maximum number given in constraint is 10^3
+        //so only 10 bits are used, remaining 22 bits are unused
+        //so we are going to use that unused 22 bits to store annther number
+        //so in one varibale two numbers are stored
+        
+        //last 10 bits -> one number
+        //the previous 10 bits -> another number
+        
+        // 0....0 0000000000 0000000000 (totally 32 bits)
+        //rem bits second no first number
+        
+        for(int i=n; i<2*n; i++)
         {
-            ans[k++] = (nums[i++]);
-            ans[k++] = (nums[j++]);
+            int secondNumber = nums[i] << 10;
+            nums[i-n] |= secondNumber;
         }
-        return ans;
+        
+        int allOnes = pow(2, 10) - 1; //(1111111111)
+        
+        for(int i=n-1; i>=0; i--)
+        {
+            int secondNumber = nums[i] >> 10;
+            int firstNumber = nums[i] & allOnes;
+            
+            nums[2*i] = firstNumber;
+            nums[2*i+1] = secondNumber;
+        }
+        
+        return nums;
         
     }
 };
