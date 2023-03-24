67bce83ed76d8c66254371a6952e5df07f44db73
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2023-03-24 10:55:28 +0530

    Time: 400 ms (67.50%), Space: 105.1 MB (84.56%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,23 +1,53 @@
 class Solution {
     
-    void dfs(int node, vector<pair<int, bool>> adj[], int n, int &count, vector<bool> &vis)
-    {
+//     void dfs(int node, vector<pair<int, bool>> adj[], int n, int &count, vector<bool> &vis)
+//     {
         
-        vis[node] = true;
-        for(auto it : adj[node])
-        {
-            if(!vis[it.first])
-            {
-                if(it.second == true)
-                {
-                    count++;
-                }
+//         vis[node] = true;
+//         for(auto it : adj[node])
+//         {
+//             if(!vis[it.first])
+//             {
+//                 if(it.second == true)
+//                 {
+//                     count++;
+//                 }
 
-                dfs(it.first, adj, n, count, vis);
-            }
-        }
+//                 dfs(it.first, adj, n, count, vis);
+//             }
+//         }
         
-    }
+//     }
+    
+        int bfs(int node, vector<pair<int, bool>> adj[], int n)
+        {
+            queue<int> q;
+            q.push(node);
+            
+            vector<bool> vis(n, false);
+            vis[node] = true;
+            
+            int count = 0;
+            
+            while(!q.empty())
+            {
+                auto city = q.front();
+                q.pop();
+                
+                for(auto it : adj[city])
+                {
+                    if(!vis[it.first])
+                    {
+                        vis[it.first] = true;
+                        count += it.second;
+                        q.push(it.first);
+                    }
+                    
+                }
+                
+            }
+            return count;
+        }
     
 public:
     int minReorder(int n, vector<vector<int>>& connections) 
@@ -31,12 +61,15 @@ public:
             adj[it[1]].push_back({it[0], false});       ////false represents extra edge for traversing
         }
         
-        vector<bool> vis(n, false);
         
-        int count = 0;
-        dfs(0, adj, n, count, vis);
+        // vector<bool> vis(n, false);
         
-        return count;
+        // int count = 0;
+        // dfs(0, adj, n, count, vis);
+        // return count;
+        
+        return bfs(0, adj, n);
+        
         
     }
 };
