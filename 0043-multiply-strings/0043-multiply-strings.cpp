8696f57b6a8cae7020704ebc549fc5516e019f72
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2023-04-13 12:18:02 +0530

    Time: 4 ms (90.04%), Space: 6.4 MB (98.60%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.cpp
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
-    string multiply(string num1, string num2) {
-        
-        //see solution tab for better understanding
-        
+    string multiply(string num1, string num2)
+    {
         if(num1 == "0" or num2 == "0")
         {
             return "0";
@@ -12,32 +10,37 @@ public:
         int n1 = num1.size();
         int n2 = num2.size();
         
+        reverse(num1.begin(), num1.end());
+        reverse(num2.begin(), num2.end());
+        
+        int i=0, j=0;
+        
         int n = n1 + n2;
         
         string ans(n, '0');
         
-        reverse(num1.begin(), num1.end());
-        reverse(num2.begin(), num2.end());
-        
-        for(int i=0; i<n2; i++)
+        for(int j=0; j<n2; j++)
         {
-            int digit1 = num2[i] - '0';
-            for(int j=0; j<n1; j++)
+            int d2 = num2[j] - '0';
+            
+            for(int i=0; i<n1; i++)
             {
-                int digit2 = num1[j] - '0';
+                int d1 = num1[i] - '0';
                 
                 int position = i + j;
                 
                 int carry = ans[position] - '0';
                 
-                int mul = (digit1 * digit2) + carry;
+                int sum = (d1 * d2) + carry;
                 
-                ans[position] = (mul%10) + '0';
+                ans[position] = (sum % 10) + '0';
                 
-                ans[position + 1] += (mul/10);
+                ans[position+1] += (sum/10);
                 
             }
+            
         }
+        
         
         while(ans.back() == '0')
         {
@@ -49,4 +52,5 @@ public:
         return ans;
         
     }
+    
 };
