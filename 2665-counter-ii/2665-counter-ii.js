c506a5fe5599f6612aeb3c45b976fc95a48f2709
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2023-05-07 13:16:35 +0530

    Time: 78 ms (8.40%), Space: 44.4 MB (81.57%) - LeetHub

--- 2665-counter-ii/2665-counter-ii.js
+++ 2665-counter-ii/2665-counter-ii.js
@@ -2,28 +2,40 @@
  * @param {integer} init
  * @return { increment: Function, decrement: Function, reset: Function }
  */
-var createCounter = function(init) {
-    let currentCount = init;
-    return {
+// var createCounter = function(init) {
+//     let currentCount = init;
+//     return {
 
-        increment : function()
-        {
-            currentCount++;
-            return currentCount;
-        },
-        decrement : function()
-        {
-            currentCount--;
-            return currentCount;
-        },
-        reset : function()
-        {
-            currentCount = init;
-            return currentCount;
-        },
-    }
+//         increment : function()
+//         {
+//             currentCount++;
+//             return currentCount;
+//         },
+//         decrement : function()
+//         {
+//             currentCount--;
+//             return currentCount;
+//         },
+//         reset : function()
+//         {
+//             currentCount = init;
+//             return currentCount;
+//         },
+//     }
         
-};
+// };
+
+var createCounter = function(init){
+    
+    let currentValue = init;
+    
+    return {
+        increment : () => ++currentValue,
+        decrement : () => --currentValue,
+        reset : () => (currentValue = init),
+    }
+    
+}
 
 /**
  * const counter = createCounter(5)
