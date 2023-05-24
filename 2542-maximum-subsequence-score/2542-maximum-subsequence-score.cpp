3d63c43590029f96b69fea212faba2a53a40cdba
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2023-05-24 19:08:08 +0530

    Time: 325 ms (31.77%), Space: 87.4 MB (83.00%) - LeetHub

---
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.cpp
@@ -0,0 +1,45 @@
+class Solution {
+    
+    static bool comp(pair<int, int> &a, pair<int, int> &b)
+    {
+        return a.second > b.second;
+    }
+    
+public:
+    long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
+        
+        int n = nums1.size();
+        
+        vector<pair<int, int>> pairs(n);
+        
+        for(int i=0; i<n; i++)
+        {
+            pairs[i] = {nums1[i], nums2[i]};
+        }
+        
+        sort(pairs.begin(), pairs.end(), comp);
+        
+        priority_queue<int, vector<int>, greater<int>> pq;
+        
+        long long topKSum = 0;
+        
+        for(int i=0; i<k; i++)
+        {
+            topKSum += (long long)pairs[i].first;
+            pq.push(pairs[i].first);
+        }
+        
+        long long answer = topKSum * pairs[k-1].second;
+        
+        for(int i=k;i<n;i++)
+        {
+            topKSum += (pairs[i].first - pq.top());
+            pq.pop();
+            pq.push(pairs[i].first);
+            answer = max(answer,topKSum*pairs[i].second);
+        }
+        
+        return answer;
+        
+    }
+};
