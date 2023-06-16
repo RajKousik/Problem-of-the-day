00cd88e4aaddd2aa673cade4a946ad07e6daecc6
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2023-06-16 21:59:05 +0530

    Time: 3 ms (38.46%), Space: 6.2 MB (22.17%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,29 +1,45 @@
 class Solution {
+    
+    int f(int n, vector<int> &dp)
+    {
+        if(n==0) return 0;
+        if(n==1 or n==2) return 1;
+        
+        if(dp[n] != -1)
+        {
+            return dp[n];
+        }
+        
+        return dp[n] = f(n-1, dp) + f(n-2, dp) + f(n-3, dp);
+    }
+    
 public:
     int tribonacci(int n) {
         
-        if(n == 0)
-        {
-            return 0;
-        }
+//         if(n == 0)
+//         {
+//             return 0;
+//         }
         
-        if(n == 1 or n == 2) return 1;
+//         if(n == 1 or n == 2) return 1;
         
         
-        int a = 0;
-        int b = 1;
-        int c = 1;
+//         int a = 0;
+//         int b = 1;
+//         int c = 1;
         
-        int sum = 0;
-        for(int i=3; i<=n; i++)
-        {
-            sum = a + b + c;
-            a = b;
-            b = c;
-            c = sum;  
-        }
+//         int sum = 0;
+//         for(int i=3; i<=n; i++)
+//         {
+//             sum = a + b + c;
+//             a = b;
+//             b = c;
+//             c = sum;  
+//         }
         
-        return sum;
+//         return sum;
+        vector<int> dp(n+1, -1);
+        return f(n, dp);
         
     }
 };
