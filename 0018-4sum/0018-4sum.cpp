3b8008b03a787cad09f6ddc925a11ecda9dd6fdf
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2023-06-22 21:11:04 +0530

    Time: 64 ms (85.98%), Space: 13.3 MB (53.29%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        //2 pointers and binary  search
+        vector<vector<int>> res;
+        if(nums.empty()){
+            return res;
+        }
+        int n = nums.size();
+        
+        sort(nums.begin(), nums.end());
+        
+        for(int i=0; i<n; i++)
+        {
+            
+            for(int j=i+1; j<n; j++)
+            {
+                
+                int low = j+1, high = n-1;
+                long long ourTarget = target - nums[j];
+                ourTarget -= nums[i];
+                
+                while(low < high)
+                {
+                    
+                    long long sum_two = nums[low] + nums[high];
+                    
+                    if(sum_two < ourTarget){
+                        low++;
+                    }
+                    else if(sum_two > ourTarget){
+                        high--;
+                    }
+                    else
+                    {
+                        vector<int> quad(4, 0);
+                        quad[0] = nums[i];
+                        quad[1] = nums[j];
+                        quad[2] = nums[low];
+                        quad[3] = nums[high];
+                        res.push_back(quad);
+                        
+                        while(low < high && nums[low]==quad[2])
+                        {
+                            low++;
+                        }
+                        
+                        while(low < high && nums[high]==quad[3])
+                        {
+                            high--;
+                        }
+                        
+                    }
+                    
+                }
+                
+                while( j+1 < n && nums[j+1] == nums[j] )
+                {
+                    j++;
+                }
+                
+            }
+            
+            while( i+1 < n && nums[i+1] == nums[i] )
+            {
+                    i++;
+            }
+            
+        }
+        
+        return res;
+    }
+};
