43bd87580832d9128005624c6c0466eb0d37a670
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2023-06-29 15:00:53 +0530

    Time: 29 ms (36.69%), Space: 13.7 MB (60.50%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,58 +1,40 @@
 class Solution {
 public:
+    
+    //just apply topo sort
+    // since topological order can only be applied for DAG(Directed Acyclic Graph)
+    // if we apply topo function and got the correct ordering of the topo sort, then this is not a cyclic grpah, so we can finish all the tasks
+    // if we are not getting correct ordering of the topo sort, then it must be a cyclic graph, so we cannot finish all the tasks
+    
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        
         int V = numCourses;
-        
         vector<int> inDegree(V, 0);
-        
         vector<int> adj[V];
-        
         for(auto it : prerequisites)
-        {
-            adj[it[0]].push_back(it[1]);
-        }
-        
-        
+            adj[it[1]].push_back(it[0]);
         for(int i=0; i<V; i++)
         {
-            for(auto it : adj[i])
-            {
-                inDegree[it]++;
-            }
+            for(auto it : adj[i]) inDegree[it]++;
         }
-        
         queue<int> q;
-        
         for(int i=0; i<V; i++)
         {
             if(inDegree[i] == 0)
-            {
                 q.push(i);
-            }
         }
-        
         vector<int> ans;
         while(!q.empty())
         {
-            
             auto node = q.front();
             q.pop();
-            
             ans.push_back(node);
-            
             for(auto it : adj[node])
             {
                 inDegree[it]--;
                 if(inDegree[it] == 0)
-                {
                     q.push(it);
-                }
             }
-            
-            
         }
-        
         return ans.size() == V;
     }
 };
