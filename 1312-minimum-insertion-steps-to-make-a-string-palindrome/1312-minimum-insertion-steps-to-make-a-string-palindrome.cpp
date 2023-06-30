536e9fdda8f1eb2cb7aee5995e7df4994de8c0cc
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2023-06-30 15:54:45 +0530

    Time: 133 ms (20.37%), Space: 28.6 MB (18.35%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,39 +1,40 @@
 class Solution {
     
-    int helper(string &s, string &t, int i, int j, vector<vector<int>> &dp)
+    int longestPalindromicSubsequence(int i, int j, string &A, string &B, vector<vector<int>> &dp)
     {
-        if(i == 0 || j == 0)
+
+        if(i == 0 or j == 0)
         {
             return 0;
         }
-        
+
         if(dp[i][j] != -1)
         {
             return dp[i][j];
         }
-        
-        if(s[i-1] == t[j-1])
+
+        if(A[i-1] == B[j-1])
         {
-            return dp[i][j] = 1 + helper(s, t, i-1, j-1, dp);
+            return dp[i][j] = 1 + longestPalindromicSubsequence(i-1, j-1, A, B, dp);
         }
-        
-        return dp[i][j] = max(helper(s, t, i-1, j, dp), helper(s, t, i, j-1, dp));
-        
+
+        return dp[i][j] = max(longestPalindromicSubsequence(i-1, j, A, B, dp),
+                  longestPalindromicSubsequence(i, j-1, A, B, dp));
+
     }
     
     
 public:
-    int minInsertions(string s)
+    int minInsertions(string A)
     {
-        string t = s;
-        
-        reverse(t.begin(), t.end());
-        
-        int n = s.size();
-        
+        int n = A.size();
+	
+        string B = A;
+        reverse(B.begin(), B.end());
+
         vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
-        
-        return n - helper(s, t, n, n, dp);
+
+        return (n- longestPalindromicSubsequence(n, n, A, B, dp)); 
         
         
         
