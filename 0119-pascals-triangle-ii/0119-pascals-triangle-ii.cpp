06b84033eca2ef6fcfbff369adbc34f38492b052
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2023-07-03 16:11:43 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (65.64%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -4,6 +4,9 @@ public:
         
         vector<int> ans(rowIndex+1, 1);
         
+        if(rowIndex <= 1)
+            return ans;
+        
         for(int i=1; i<rowIndex; i++)
         {
             for(int j=i; j>0; j--)
