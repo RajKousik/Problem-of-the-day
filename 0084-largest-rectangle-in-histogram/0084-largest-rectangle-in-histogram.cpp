c79e5ee946ffb17116f35c51c54e859176048480
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2023-07-04 19:27:38 +0530

    Time: 218 ms (53.76%), Space: 81.4 MB (57.36%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,36 +1,71 @@
 class Solution {
+private:
+    
+    vector<int> findNextSmaller(vector<int> &heights, int n)
+    {
+        vector<int> nextSmaller(n, 0);
+        stack<int> st;
+        
+        for(int i=n-1; i>=0; i--)
+        {
+            while(!st.empty() and heights[st.top()] >= heights[i])
+            {
+                st.pop();
+            }
+            if(!st.empty())
+            {
+                nextSmaller[i] = st.top() - 1;
+            }
+            else
+            {
+                nextSmaller[i] = n-1;
+            }
+            st.push(i);
+        }
+        return nextSmaller;
+    }
+    
+    vector<int> findPreviousSmaller(vector<int> &heights, int n)
+    {
+        vector<int> prevSmaller(n, 0);
+        stack<int> st;
+        
+        for(int i=0; i<n; i++)
+        {
+            while(!st.empty() and heights[st.top()] >= heights[i])
+            {
+                st.pop();
+            }
+            if(!st.empty())
+            {
+                prevSmaller[i] = st.top() + 1;
+            }
+            else
+            {
+                prevSmaller[i] = 0;
+            }
+            st.push(i);
+        }
+        return prevSmaller;   
+    }
+    
 public:
     int largestRectangleArea(vector<int>& heights) {
         
         int n = heights.size();
-        vector<int> previousSmaller(n), nextSmaller(n);
-        stack<int> st;
-        for(int i=0; i<n; i++) {
-            while(!st.empty() and heights[st.top()] >= heights[i])
-                st.pop();
-            if(!st.empty())
-                previousSmaller[i] = st.top() + 1;
-            else
-                previousSmaller[i] = 0;
-            st.push(i);
-        }
-        while(!st.empty()) st.pop();
-        for(int i=n-1; i>=0; i--){
-            while(!st.empty() and heights[st.top()] >= heights[i])
-                st.pop();
-            if(!st.empty())
-                nextSmaller[i] = st.top() - 1;
-            else
-                nextSmaller[i] = n-1;
-            st.push(i);
-        }
-        int maxArea = 0;
+        
+        // int previousSmaller(n, 0), nextSmaller(n, 0);
+        
+        vector<int> previousSmaller = findPreviousSmaller(heights, n);
+        vector<int> nextSmaller = findNextSmaller(heights, n);
+        
+        int ans = 0;
         for(int i=0; i<n; i++)
         {
-            int currentArea = heights[i] * (nextSmaller[i] - previousSmaller[i] + 1);
-            maxArea = max(maxArea, currentArea);
+            ans = max(ans, (heights[i] * (nextSmaller[i] - previousSmaller[i] + 1)));
+            // cout<<previousSmaller[i]<<" "<<nextSmaller[i]<<endl;
         }
-        return maxArea;
         
+        return ans;
     }
 };
