be37c16fb0b6fe586e91f29b459dd36725310531
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2023-07-04 12:07:28 +0530

    Time: 27 ms (13.60%), Space: 20.3 MB (68.58%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -2,37 +2,57 @@ class Solution {
 public:
     bool canPlaceFlowers(vector<int>& arr, int n) {
         
-        if(n == 0) return true;
+//         if(n == 0) return true;
         
+//         int len = arr.size(), flowersPlaced=0;
+
+//         if(arr[0] == 0 and len == 1)
+//             return true;
+//         else if(arr[0] == 1 and len==1)
+//             return false;
+        
+//         if(arr[0] == 0 and arr[1] != 1)
+//         {
+//             arr[0] = 1;
+//             flowersPlaced++;
+//         }
+        
+//         if(arr[len-1] == 0 and arr[len-2] != 1)
+//         {
+//             arr[len-1] = 1;
+//             flowersPlaced++;
+//         }
+        
+//         for(int i=1; i<len-1; i++)
+//         {
+//             if(arr[i] == 0 and arr[i-1] != 1 and arr[i+1] != 1)
+//             {
+//                 flowersPlaced++;
+//                 arr[i] = 1;
+//             }
+//         }
         int len = arr.size(), flowersPlaced=0;
-
-        if(arr[0] == 0 and len == 1)
-            return true;
-        else if(arr[0] == 0 and len==1)
-            return false;
-
-        
-        
-        if(arr[0] == 0 and arr[1] != 1)
+        for(int i=0; i<len; i++)
         {
-            arr[0] = 1;
-            flowersPlaced++;
-        }
-        
-        if(arr[len-1] == 0 and arr[len-2] != 1)
-        {
-            arr[len-1] = 1;
-            flowersPlaced++;
-        }
-        
-        for(int i=1; i<len-1; i++)
-        {
-            if(arr[i] == 0 and arr[i-1] != 1 and arr[i+1] != 1)
+            if(arr[i] == 0)
             {
-                flowersPlaced++;
-                arr[i] = 1;
+                bool isLeftSafe = (i==0) or (arr[i-1] == 0);
+                bool isRightSafe = (i==len-1) or (arr[i+1] == 0);
+                
+                
+                if(isLeftSafe and isRightSafe)
+                {
+                    flowersPlaced++;
+                    arr[i] = 1;
+                    if(flowersPlaced >= n)
+                    {
+                        return true;
+                    }
+                }
+                
             }
         }
+        
         return flowersPlaced >= n;
     }
 };
