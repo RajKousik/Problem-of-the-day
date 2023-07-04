d9969881f47176021f9d3fc88cf9830257aa605b
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2023-07-04 22:56:22 +0530

    Time: 12 ms (93.19%), Space: 10.7 MB (11.34%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,49 +1,85 @@
 class Solution {
+    
+//     int dfs(int row, int col, vector<vector<int>>& matrix, int n, vector<vector<int>> &dp)
+//     {
+//         if(row >= n or row < 0 or col < 0 or col >= n)
+//         {
+//             return 1e5;
+//         }
+        
+//         if(row == n-1)
+//         {
+//             return matrix[row][col];
+//         }
+        
+//         if(dp[row][col] != -1)
+//         {
+//             return dp[row][col];
+//         }
+        
+//         int down = dfs(row+1, col, matrix, n, dp);
+//         int downRight = dfs(row+1, col+1, matrix, n, dp);
+//         int downLeft = dfs(row+1, col-1, matrix, n, dp);
+        
+//         return dp[row][col] = matrix[row][col] + min(down, min(downRight, downLeft));
+        
+//     }
+    
 public:
+//     int minFallingPathSum(vector<vector<int>>& matrix) {
+        
+//         int ans = 1e5;
+        
+//         int n = matrix.size();
+        
+//         vector<vector<int>> dp(n, vector<int>(n, -1));
+        
+//         for(int col=0; col<n; col++)
+//         {
+//             ans = min(ans, dfs(0, col, matrix, n, dp));
+//         }
+//         return ans;
+        
+//     }
     
-    int dfs(int i, int j, vector<vector<int>> &matrix, int n, vector<vector<int>> &dp)
-    {
+       int minFallingPathSum(vector<vector<int>>& matrix) 
+       {
         
-        if(i<0 or i>=n or j<0 or j>=n)
-        {
-            return 1e5;     //as max no of n is 100, and max value is 100, so max sum will be 10000. so we to have choose greater than 10^4, so we have taken 10^5.
-            //see Constraints
+           int ans = 1e5;
+  
+           int n = matrix.size();
+
+           vector<vector<int>> dp(n, vector<int>(n, 0));
+
+           for(int j=0; j<n; j++)
+           {
+               dp[n-1][j] = matrix[n-1][j];
+           }
+           
+           for(int row=n-2; row>=0; row--)
+           {
+               for(int col=n-1; col>=0; col--)
+               {
+                    int down = dp[row+1][col];
+                   
+                    int downRight = 1e5;
+                    if(col+1 < n)
+                        downRight = dp[row+1][col+1];
+                    int downLeft = 1e5;
+                    if(col-1 >= 0)
+                        downLeft = dp[row+1][col-1];
+
+                    dp[row][col] = matrix[row][col] + min(down, min(downRight, downLeft));
+               }
+           }
+            
+           for(int j=0; j<n; j++)
+           {
+               ans = min(ans, dp[0][j]);
+           }
+           
+           return ans;
+        
         }
-        
-        if(dp[i][j] != -1)
-        {
-            return dp[i][j];
-        }
-        
-        if(i == n-1)
-        {
-            return matrix[i][j];
-        }
-        
-        int down = dfs(i+1, j, matrix, n, dp);
-        
-        int downLeft = dfs(i+1, j-1, matrix, n, dp);   
-       
-        int downRight = dfs(i+1, j+1, matrix, n, dp);   
-        
-        return dp[i][j] = matrix[i][j] + min(down, min(downLeft, downRight));
-        
-    }
     
-    int minFallingPathSum(vector<vector<int>>& matrix) {
-        
-        int n = matrix.size();
-        
-        int ans = 1e5;
-    
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        
-        for(int j=0; j<n; j++)
-        {
-            ans = min(ans, dfs(0, j, matrix, n, dp));
-        }
-        
-        return ans;
-        
-    }
 };
