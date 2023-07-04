9f13483378ea9b22b263fa6885d1bdc982af2252
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2023-07-04 08:40:46 +0530

    Time: 13 ms (28.44%), Space: 9.3 MB (98.52%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        int ans = 0, countOdd = 0, countEven = 0;
+        
+        for(int i=0; i<32; i++)
+        {
+            countOdd = 0, countEven = 0;
+            for(int j=0; j<nums.size(); j++)
+            {
+                if((nums[j] & (1 << i)) != 0) //then it is 1 bit number
+                {
+                    countOdd++;
+                }
+                else
+                {
+                    countEven++;
+                }
+            }
+            if(countOdd % 3)
+            {
+                ans = ans | (1 << i);   
+            }
+        }
+        return ans;
+        
+    }
+};
