7377ed25fd6834e86cf0c485e5336e029c1f0785
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2023-07-19 19:22:36 +0530

    Time: 20 ms (49.90%), Space: 17.4 MB (17.42%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,60 +1,33 @@
 class Solution {
-    
 private:
-    
-    bool isOverlapping(vector<int> &a, vector<int> &b)
-    {
-        return min(a[1],b[1]) - max(a[0],b[0]) >= 0;
+    bool isOverlapping(vector<int> &a, vector<int> &b) {
+        // return min(a[1],b[1]) - max(a[0],b[0]) >= 0;
+        return b[0] <= a[1];
     }
-    
-    vector<int> mergeIntervals(vector<int> &a, vector<int> &b)
-    {
+    vector<int> mergeIntervals(vector<int> &a, vector<int> &b) {
         return {min(a[0], b[0]), max(a[1], b[1])};
     }
-    
-    void insertInterval(vector<vector<int>>& intervals, vector<int>& newInterval)
-    {
-        
-        auto index = upper_bound(intervals.begin(), intervals.end(), newInterval) - intervals.begin();
-        
+    void insertInterval(vector<vector<int>>& intervals, vector<int>& newInterval) {   
+        auto index = upper_bound(intervals.begin(), intervals.end(), newInterval) - intervals.begin(); 
         if(index != intervals.size())
-        {
             intervals.insert(intervals.begin() + index, newInterval);
-        }
         else
-        {
             intervals.push_back(newInterval);
-        }
-        
-        
     }
-    
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {   
         insertInterval(intervals, newInterval);
-        
         vector<vector<int>> ans;
-        
         int n = intervals.size();
-        
-        for(int i=0; i<n; i++)
-        {
-            
-            vector<int> currentInterval = intervals[i];
-            
-            while(i < n and isOverlapping(currentInterval, intervals[i]))
-            {
+        for(int i=0; i<n; i++) {
+            vector<int> currentInterval = intervals[i];   
+            while(i < n and isOverlapping(currentInterval, intervals[i])) {
                 currentInterval = mergeIntervals(currentInterval, intervals[i]);
                 i++;
             }
-            
             i--;
-            
             ans.push_back(currentInterval);
         }
-        
         return ans;
-        
     }
 };
