9e4670fd6c5b9ca9f7d22db0c89dbb436678a9f1
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2023-07-24 18:45:08 +0530

    Time: 4 ms (40.18%), Space: 5.9 MB (92.36%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,47 +1,25 @@
 class Solution {
 private:
-    
     double calculatePower(double x, long long n)
     {
-        
         if(n == 0) return 1.0;
-        
         if(n < 0)
-        {
             return calculatePower(1/x, -1*n);
-        }
-        
         if(n % 2 == 1)
-        {
             return x * calculatePower(x*x, n/2);
-        }
         else
-        {
             return calculatePower(x*x, n/2);
-        }
-        
         return 0.0;
-        
     }
-    
 public:
     double myPow(double x, int n) {
-        
         //binary exponential
-        
         // 2^100 == 4^50 == 16^25 == 16*(16^24) == 
         //     16^24 == 256 ^ 12 == (256*256)^6 == (x*x)^3 == x * (x*x)^1
-        //square the digit and half the power
-        
+        //******** Square the Digit and Half the Power *******//
         //handle negative cases
-        // x^-n => 1/(x^-n)
-        //if n = -8
-        // 1/n^-(-8)
-        //1/n^8
-        
+        // x^-n => 1/(x^-n) ===> if n = -8 ===> 1/n^-(-8) ===> 1/n^8
         //as we r using negative numbers, there may be an overflow for integer, so one should use long long
-        
         return calculatePower(x, (long long)n);
-        
     }
 };
