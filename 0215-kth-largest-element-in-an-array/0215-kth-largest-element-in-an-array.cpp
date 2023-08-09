492330bccfa41c5857c06e2227d0cbc97b2889dd
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2023-08-09 20:02:45 +0530

    Time: 66 ms (98.94%), Space: 67.1 MB (5.07%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,7 +1,44 @@
 class Solution {
 public:
+    
+    int quickSelect(vector<int> &nums, int k)
+    {
+        int pivot = nums[rand() % nums.size()];
+        
+        vector<int> low, mid, high;
+        
+        for(int num : nums)
+        {
+            if(num > pivot)
+            {
+                low.push_back(num);
+            }
+            else if(num < pivot)
+            {
+                high.push_back(num);
+            }
+            else
+            {
+                mid.push_back(num);
+            }
+        }
+        
+        if(k <= low.size())
+        {
+            return quickSelect(low, k);
+        }
+        else if(low.size() + mid.size() < k)
+        {
+            return quickSelect(high, k - low.size() - mid.size());
+        }
+
+        return pivot;
+        
+    }
+    
+    
     int findKthLargest(vector<int>& nums, int k) {
-//         priority_queue<int, vector<int>, greater<int>> pq;
+        // priority_queue<int, vector<int>, greater<int>> pq;
         
 //         for(auto it : nums)
 //         {
@@ -13,19 +50,21 @@ public:
 //         }
         
 //         return pq.top();
-        priority_queue<int> pq;
+//         priority_queue<int> pq;
         
-        for(auto it : nums)
-        {
-            pq.push(it);
-        }
+//         for(auto it : nums)
+//         {
+//             pq.push(it);
+//         }
         
-        while(--k)
-        {
-            pq.pop();
-        }
+//         while(--k) //for(int i=0; i<k-1; i++)
+//         {
+//             pq.pop();
+//         }
         
-        return pq.top();
+//         return pq.top();
+        
+        return quickSelect(nums, k);
             
         
     }
