680287f03999068a20a3753406742c46b517bca0
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2023-08-10 18:24:27 +0530

    Time: 174 ms (87.80%), Space: 74 MB (21.40%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,75 +10,49 @@
  */
 class Solution {
 public:
-    
-    ListNode* findMid(ListNode* head)
-    {
+    ListNode* findMid(ListNode* head) {
         ListNode* fast = head->next, *slow = head;
-        
         while(fast and fast->next)
         {
             fast = fast->next->next;
             slow = slow->next;
         }
         return slow;
-        // return head;
     }
-    
     ListNode* merge(ListNode* head1, ListNode* head2)
     {
         if(!head1) return head2;
         if(!head2) return head1;
-        
         ListNode* res = new ListNode(-1);
         ListNode* ptr = res;
-        
-        while(head1 and head2)
-        {
-            if(head1->val <= head2->val)
-            {
+        while(head1 and head2) {
+            if(head1->val <= head2->val) {
                 ptr->next = head1;
                 head1 = head1->next;
             }
-            else
-            {
+            else {
                 ptr->next = head2;
                 head2 = head2->next;
             }
             ptr = ptr->next;
         }
-        while(head1)
-        {
+        if(head1) {
             ptr->next = head1;
-            head1 = head1->next;
-            ptr = ptr->next;
         }
-        while(head2)
-        {
+        if(head2) {
             ptr->next = head2;
-            head2 = head2->next;
-            ptr = ptr->next;
         }
         return res->next;
-        // return NULL;
     }
-    
     ListNode* sortList(ListNode* head) {
-        
         if(head==NULL or head->next==NULL)
-        {
             return head;
-        }
-        
         ListNode* mid = findMid(head);
-        
         ListNode* left = head;
         ListNode* right = mid->next;
-        
         mid->next = NULL;
-        
         left = sortList(left);
         right = sortList(right);
-        
         return merge(left, right);
     }
 };
